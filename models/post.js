0db2425c4ea98a9c09c9633c917ce95667b63ab0
Jordan <jordansampson23@gmail.com>
2023-09-15 13:49:13 -0500

    Add user

--- models/post.js
+++ models/post.js
@@ -5,6 +5,7 @@ const PostSchema = new Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
     published: { type: Boolean, default: false },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     timestamp: { type: Date, default: Date.now },
     allowComments: { type: Boolean, default: true },
     comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
