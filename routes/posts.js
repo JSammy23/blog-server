1791f5c143087f70e1086904681eac0ceed41def
Jordan <jordansampson23@gmail.com>
2023-09-15 13:49:30 -0500

    Protect admin routes.

--- routes/posts.js
+++ routes/posts.js
@@ -1,30 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+const mware = require('../config/middleware');
 
 const postController = require('../controllers/postController');
 const commentsController = require('../controllers/commentController');
 
-// GET all posts
+// GET all published posts
 router.get('/', postController.index);
+
+// GET all posts
+router.get('/all', passport.authenticate('jwt', {session: false}), mware.ensureAdmin, postController.getAllPosts);
 
 // GET specific post
 router.get('/:id', postController.get_post);
 
 // POST create post
-router.post('/', postController.create_post);
+router.post('/', passport.authenticate('jwt', {session: false}), mware.ensureAdmin, postController.create_post);
 
 // PUT update post
-router.put('/:id', postController.update_post);
+router.put('/:id', passport.authenticate('jwt', {session: false}), mware.ensureAdmin, postController.update_post);
 
 // DELETE post
-router.delete('/:id', postController.delete_post);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), mware.ensureAdmin, postController.delete_post);
 
 ///// Comment Routes /////
 
 // POST create comment
-router.post('/:id/comments', commentsController.create_comment);
+router.post('/:id/comments', passport.authenticate('jwt', {session: false}), commentsController.create_comment);
 
 // DELETE comment
-router.delete('/:id/comment/:commentId', commentsController.delete_comment);
+router.delete('/:id/comment/:commentId', passport.authenticate('jwt', {session: false}), commentsController.delete_comment);
 
 module.exports = router;
