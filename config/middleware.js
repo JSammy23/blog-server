2ccbeb5b0ac3a7db16bcc298c1f26637da299c45
Jordan <jordansampson23@gmail.com>
2023-09-15 13:48:41 -0500

    EnsyureAdmin function.

--- config/middleware.js
+++ config/middleware.js
@@ -5,3 +5,11 @@ exports.ensureAdminOrSelf = (req, res, next) => {
         return res.status(403).json({ message: 'Unauthorized' });
     }
 };
+
+exports.ensureAdmin = (req, res, next) => {
+    if (req.user.role === 'admin') {
+        return next();
+    } else {
+        return res.status(403).json({ message: 'Unauthorized' });
+    };
+};
